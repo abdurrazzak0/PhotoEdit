30b062d3d2aca8ba3558f884ec6ced215197e8c4
abdurrazzak0 <razzak6366@gmail.com>
2023-09-12 17:30:39 +0600

    first commit

--- PhotoEdit/ViewController.swift
+++ PhotoEdit/ViewController.swift
@@ -4,16 +4,68 @@
 //
 //  Created by Abdur Razzak on 5/9/23.
 //
-
+import CropViewController
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, CropViewControllerDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+        button.setTitle("Pick Photo", for: .normal)
+        view.addSubview(button)
+        button.setTitleColor(.white, for: .normal)
+        button.center = view.center
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        
     }
-
-
+    
+    @objc func didTapButton() {
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.originalImage] as? UIImage else {
+            return
+        }
+        picker.dismiss(animated: true)
+        
+        showCrop(image: image)
+    }
+    
+    func showCrop(image: UIImage) {
+        let vc = CropViewController(croppingStyle: .default, image: image)
+        vc.aspectRatioPreset = .presetSquare
+        vc.aspectRatioLockEnabled = true
+        vc.doneButtonColor = .systemRed
+        vc.cancelButtonColor = .systemRed
+        vc.toolbarPosition = .bottom
+        vc.doneButtonTitle = "Done"
+        vc.cancelButtonTitle = "Back"
+        vc.delegate = self
+        present(vc, animated: true)
+    }
+    func cropViewController(_ cropViewController: CropViewController, didFinishCancelled cancelled: Bool) {
+        cropViewController.dismiss(animated: true)
+    }
+    func cropViewController(_ cropViewController: CropViewController, didCropImageToRect cropRect: CGRect, angle: Int) {
+        cropViewController.dismiss(animated: true)
+        print("Did crop")
+        
+        let imageVIew = UIImageView(frame: view.frame)
+        imageVIew.contentMode = .scaleAspectFit
+       // imageVIew.image = image
+        view.addSubview(imageVIew)
+        
+    }
 }
-
